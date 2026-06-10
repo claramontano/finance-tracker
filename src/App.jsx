3e43303a31ebce4460d12371f3b79bcc643de771
claramontano <montanorodriguezclara@gmail.com>
2026-06-10 12:03:30 +0200

    chore: init Finance Tracker with Vite + React + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-950 text-white p-8">
+      <h1 className="text-4xl font-bold text-blue-400">Finance Tracker</h1>
+    </div>
+  )
+}
+
+export default App
